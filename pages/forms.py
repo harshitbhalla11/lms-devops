42e6d5f8c1994d608acd26f38201df1f52c6346f
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-04-04 19:39:47 +0100

    check

--- pages/forms.py
+++ pages/forms.py
@@ -8,15 +8,6 @@ class ExamForm(forms.ModelForm):
     """
     Exam form
     """
- 
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    def save_exam(self):
-        """
-        Save exam method
-        """
-        pass
 
     class Meta:
         """
