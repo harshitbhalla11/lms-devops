a78298ee6860f537f5b08ca2b96265b302d66f0b
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-04-04 17:27:36 +0100

    change

--- pages/forms.py
+++ pages/forms.py
@@ -1,3 +1,6 @@
+"""
+    forms.py
+"""
 from django import forms
 from .models import Exam
 
@@ -8,7 +11,10 @@ class ExamForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
     class Meta:
+        """
+        exam form fields
+        """
         model = Exam
         fields = ['title', 'description', 'duration', 'visibility']
