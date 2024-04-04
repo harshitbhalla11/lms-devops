066e39ed0ef5a5de61bbb450464d83d91dd38e5c
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-04-04 16:58:31 +0100

    fix py

--- lms/urls.py
+++ lms/urls.py
@@ -1,38 +1,22 @@
-"""
-URL configuration for team_talk project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from pages.views import createExam, Examlist, create_exam, exam, add_question, results, studentExamList, examination, submit_exam, update_exam, myResults
 from .views import entry_views
-from pages.views import createExam, Examlist, create_exam, exam, add_question, results, studentExamList,examination, submit_exam,update_exam, myResults
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(("accounts.urls","signupLogin"),"signupLogin")),
+    path('', include(("accounts.urls", "signupLogin"), "signupLogin")),
     path('', entry_views, name='home'),
     path('createExam/', createExam, name='createExam'),
     path('Examlist/', Examlist, name='Examlist'),
     path('exam/<int:id>/', exam, name='exam'),
-    path('results/<int:id>/', results,name='results'),
-    path('openExam/', studentExamList ,name='studentExamList'),
-    path('examination/<int:id>/', examination ,name='examination'),
-    path('myResults', myResults ,name='myResults'),
+    path('results/<int:id>/', results, name='results'),
+    path('openExam/', studentExamList, name='studentExamList'),
+    path('examination/<int:id>/', examination, name='examination'),
+    path('myResults/', myResults, name='myResults'),
 
-    path('create_exam/', create_exam, name='create_exam'),    
-    path('add_question/<int:exam_id>/',add_question, name='add_question'),
+    path('create_exam/', create_exam, name='create_exam'),
+    path('add_question/<int:exam_id>/', add_question, name='add_question'),
     path('submit_exam/<int:exam_id>/', submit_exam, name='submit_exam'),
     path('update_exam/<int:exam_id>/', update_exam, name='update_exam'),
-
-]   
+]
