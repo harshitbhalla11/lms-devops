0eac94afebabc6650ba7b9d3f989ea54c875ed29
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-04-04 17:10:54 +0100

    add changes to view

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+"""
+Defines views for the 'lms' app.
+"""
+
+from django.shortcuts import render
 
 def entry_views(request):
+    """
+    Renders home/landing based on whether the user is authenticated.
+    """
     if request.user.is_authenticated:
-        return render(request, 'home-page.html', {})    
-    else:
-        return render(request, 'landing-page.html', {})
+        return render(request, 'home-page.html', {})
+    return render(request, 'landing-page.html', {})
      
