5b6bbe949a638de5d3fcdead01e87c63f35c682c
EC2 Default User <ec2-user@ip-172-31-44-98.eu-west-1.compute.internal>
2024-04-06 23:37:07 +0000

    django change

--- lms/settings.py
+++ lms/settings.py
@@ -16,8 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
