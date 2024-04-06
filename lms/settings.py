f7e8d7c090c02e44725dab07f03afda32f6d3401
EC2 Default User <ec2-user@ip-172-31-44-98.eu-west-1.compute.internal>
2024-04-06 17:45:40 +0000

    change

--- lms/settings.py
+++ lms/settings.py
@@ -16,7 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
